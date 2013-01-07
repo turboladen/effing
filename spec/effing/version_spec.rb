cae338c52e559006a525039de892aa2b0e1d3547
Steve Loveless <steve.loveless@gmail.com>
2013-01-07 15:06:10 -0800

    Fixed effing specs (lol)

---
+++ spec/effing/version_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+require 'effing/version'
+
+
+describe Effing::VERSION do
+  it { should == '0.1.0' }
+end
--- spec/effing_spec.rb
+++ spec/effing_spec.rb
@@ -1,9 +1,8 @@
 require_relative 'spec_helper'
 require 'effing'
 
-describe Effing do
-  specify { Effing::VERSION.should == '0.1.0' }
 
+describe Effing do
   describe "#initialize" do
     it "should do some stuff" do
       pending "FIXME"
