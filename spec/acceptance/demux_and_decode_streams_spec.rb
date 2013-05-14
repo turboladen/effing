1150accb712923e3b97c017e2761fc03c8e9ea8d
Steve Loveless <steve.loveless@gmail.com>
2013-05-14 15:52:48 -0700

    Fix formatting

--- spec/acceptance/demux_and_decode_streams_spec.rb
+++ spec/acceptance/demux_and_decode_streams_spec.rb
@@ -4,9 +4,9 @@ require 'effing/stream_reader'
 
 Effing.log = false
 
-describe "Demuxing streams" do
-  shared_examples_for "a video demuxer" do
-    it "demuxes without errors" do
+describe 'Demuxing streams' do
+  shared_examples_for 'a video demuxer' do
+    it 'demuxes without errors' do
       received_packets = []
       stream_reader = Effing::StreamReader.new(file, :video)
       stream_reader.demux do |packet|
@@ -18,8 +18,8 @@ describe "Demuxing streams" do
     end
   end
 
-  shared_examples_for "an audio demuxer" do
-    it "demuxes without errors" do
+  shared_examples_for 'an audio demuxer' do
+    it 'demuxes without errors' do
       received_packets = []
       stream_reader = Effing::StreamReader.new(file, :audio)
       stream_reader.demux do |packet|
@@ -31,11 +31,11 @@ describe "Demuxing streams" do
     end
   end
 
-    Dir[File.dirname(__FILE__) + "/../support/*"].each do |file|
-      context file do
-        let(:file) { file }
-        it_should_behave_like "a video demuxer"
-        it_should_behave_like "an audio demuxer"
-      end
+  Dir[File.dirname(__FILE__) + '/../support/*'].each do |file|
+    context file do
+      let(:file) { file }
+      it_should_behave_like 'a video demuxer'
+      it_should_behave_like 'an audio demuxer'
     end
+  end
 end
